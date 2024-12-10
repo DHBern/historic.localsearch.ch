d45078a5cb69a43b03e26db756dcfe3805b9e112
Sebi <sebastian@flick.solutions>
2024-12-10 11:34:52 +0100

    enhance demo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,43 @@
 import type { PageServerLoad } from './$types';
 import { db } from '$lib/server/db';
 import { records } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm';
+import { eq, like } from 'drizzle-orm';
+
+const names = [
+	'Flick',
+	'Hodel',
+	'Bürki',
+	'Loosli',
+	'Scheller',
+	'Schmid',
+	'Schneider',
+	'Schweizer',
+	'Stalder',
+	'Zürcher',
+	'Zimmermann',
+	'Zbinden',
+	'Zumbrunn',
+	'Zumstein',
+	'Zumwald',
+	'Mauch',
+	'Maurer',
+	'Meier',
+	'Meister',
+	'Merz',
+	'Meyer',
+	'Michel',
+	'Müller'
+];
 
 export const load = (async () => {
-	const entries = await db
-		.select()
-		.from(records)
-		.where(eq(records.entityname1, 'Hodel'))
-		.limit(10000);
-	return { data: entries };
+	const promises = names.map((name) => {
+		// const entries = db.select().from(records).where(eq(records.entityname1, name)).limit(10000);
+		const entries = db
+			.select()
+			.from(records)
+			.where(like(records.aggregatedcontent, `%${name}%`))
+			.limit(10000);
+		return entries;
+	});
+	return { names: names, promises: promises };
 }) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,18 @@
 <script>
 	const { data } = $props();
-	console.log(data);
 </script>
 
-{JSON.stringify(data)}
+{data.names.length}
+
+<h1>promises</h1>
+<ul>
+	{#each data.promises as promise}
+		<li>
+			{#await promise}
+				loading
+			{:then data}
+				{JSON.stringify(data[0])} Total: {data?.length}
+			{/await}
+		</li>
+	{/each}
+</ul>

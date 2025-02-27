f808e908b9ba264419aee5e93a55b77134605930
flicksolutions <sebastian@flick.solutions>
2025-02-27 15:59:49 +0100

    add i18n strings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import * as m from '$lib/paraglide/messages.js';
 	import '../app.css';
 	let { children } = $props();
+	import Header from './Header.svelte';
 </script>
 
 <ParaglideJS {i18n}>
-	<header>
-		<h1>historic.localsearch</h1>
-		<nav>
-			<a href="/">{}</a>
-		</nav>
-	</header>
+	<Header />
 	{@render children()}
 </ParaglideJS>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages.js';
+</script>
+
+<header>
+	<h1>historic.localsearch</h1>
+	<nav>
+		<a href="/">{m.aqua_long_worm_gasp()}</a>
+	</nav>
+</header>

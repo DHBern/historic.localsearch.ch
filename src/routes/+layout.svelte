89c6ad809bbe7ad47705236e4f50d809a80db496
flicksolutions <sebastian@flick.solutions>
2025-02-27 14:43:14 +0100

    changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-	import { i18n } from "$lib/i18n";
-	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import * as m from '$lib/paraglide/messages.js';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
+	<header>
+		<h1>historic.localsearch</h1>
+		<nav>
+			<a href="/">{}</a>
+		</nav>
+	</header>
 	{@render children()}
 </ParaglideJS>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,3 @@
 <script>
 	const { data } = $props();
 </script>
-
-{data.names.length}
-
-<h1>promises</h1>
-<ul>
-	{#each data.promises as promise}
-		<li>
-			{#await promise}
-				loading
-			{:then data}
-				{JSON.stringify(data[0])} Total: {data?.length}
-			{/await}
-		</li>
-	{/each}
-</ul>

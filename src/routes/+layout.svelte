3b452deaeffdc3525074efbf367fc2e9b9136ab7
flicksolutions <sebastian@flick.solutions>
2025-02-28 22:50:12 +0100

    upgrade paraglide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,20 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	let { children } = $props();
 	import Header from './Header.svelte';
 	import { Modals } from 'svelte-modals';
 </script>
 
-<ParaglideJS {i18n}>
-	<Header />
-	{@render children()}
-	<Modals>
-		<!-- shown when any modal is opened -->
-		{#snippet backdrop({ close })}
-			<!-- svelte-ignore a11y_click_events_have_key_events -->
-			<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-			<div role="banner" class="backdrop" onclick={() => close()}></div>
-		{/snippet}
-	</Modals>
-</ParaglideJS>
+<Header />
+{@render children()}
+<Modals>
+	<!-- shown when any modal is opened -->
+	{#snippet backdrop({ close })}
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+		<div role="banner" class="backdrop" onclick={() => close()}></div>
+	{/snippet}
+</Modals>
 
 <style>
 	.backdrop {

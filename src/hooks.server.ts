ac4f5342161c62258d71bda8f1441d722716f3da
flicksolutions <sebastian@flick.solutions>
2025-03-11 21:30:54 +0100

    revert the edge-fixes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
-import { Buffer } from 'buffer';
-import { env } from '$env/dynamic/private';
-
-if (typeof globalThis.Buffer === 'undefined') {
-	globalThis.Buffer = Buffer;
-}
-
-if (typeof globalThis.process === 'undefined') {
-	globalThis.process = { env };
-}
 
 // creating a handle to use the paraglide middleware
 const paraglideHandle: Handle = ({ event, resolve }) =>

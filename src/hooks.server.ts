6ac53e27cc0982e9baf1ae09b2ff1a41c990a154
flicksolutions <sebastian@flick.solutions>
2025-03-11 21:21:37 +0100

    test if missing process can be fixed the same way

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,14 @@
 import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 import { Buffer } from 'buffer';
+import { env } from '$env/dynamic/private';
 
 if (typeof globalThis.Buffer === 'undefined') {
 	globalThis.Buffer = Buffer;
+}
+
+if (typeof globalThis.process === 'undefined') {
+	globalThis.process = { env };
 }
 
 // creating a handle to use the paraglide middleware

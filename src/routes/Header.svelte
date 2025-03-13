bd81690a35bba3cbbf2473b65a95f2f341764029
flicksolutions <sebastian@flick.solutions>
2025-03-13 21:50:03 +0100

    add styles

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,18 +8,48 @@
 	}
 </script>
 
-<header>
-	<h1><a href={localizeHref('/')}>historic.localsearch</a></h1>
-	<nav>
-		<button onclick={() => handleClick('about')}>{m.blue_short_bird_fly()}</button>
-		<button onclick={() => handleClick('help')}>{m.teal_sour_berry_pick()}</button>
+<header class="m-7 flex justify-between">
+	<h1 class="header-content">
+		<a href={localizeHref('/')}>historic.localsearch</a>
+	</h1>
+	<nav class="flex gap-8">
+		<button
+			class="h-10 rounded-full border px-4 drop-shadow-sm"
+			onclick={() => handleClick('about')}>{m.blue_short_bird_fly()}</button
+		>
+		<button class="h-10 rounded-full border px-4 drop-shadow-sm" onclick={() => handleClick('help')}
+			>{m.teal_sour_berry_pick()}</button
+		>
 		{#each locales as locale}
 			{@const currentLocale = getLocale()}
 			{#if currentLocale !== locale}
-				<button onclick={() => setLocale(locale)}>
+				<button
+					class="h-10 rounded-full border px-4 drop-shadow-sm"
+					onclick={() => setLocale(locale)}
+				>
 					{locale}
 				</button>
 			{/if}
 		{/each}
 	</nav>
 </header>
+
+<style>
+	.header-content {
+		margin: 0;
+		padding: 0;
+		font-family: 'Favorit Bold', Helvetica, Arial, Geneva, Verdana, Tahoma, sans-serif;
+		font-size: 1.5rem;
+		line-height: 1.75rem;
+		color: #fff;
+		text-shadow: 0px 0px 5px #000;
+	}
+	@media screen and (min-width: 710px) {
+		.header-content {
+			font-size: 2rem;
+		}
+	}
+	button {
+		text-shadow: 0px 0px 5px #000;
+	}
+</style>

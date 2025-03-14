c72380bdecc8d03a86f86f3c893d4d4fce780041
flicksolutions <sebastian@flick.solutions>
2025-03-14 17:32:23 +0100

    finish styling

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -4,6 +4,7 @@
 	import typewriter from '$lib/typewriter';
 	import type { PageData, ActionData } from './$types';
 	import pdfIds from './pdfIds.json';
+	import { nonpassive } from 'svelte/legacy';
 
 	let { form }: { data: PageData; form: ActionData } = $props();
 	let showresults = $state(20);
@@ -228,75 +229,91 @@
 		{#each Object.keys(visibleResults) as year}
 			<h2 class="text-4xl">{year}</h2>
 			{#each visibleResults[Number(year)] as result}
-				<div class="border-t-2 pb-4">
-					{#if result.entityname1}
-						<p>
-							<span class="font-bold">{result?.entityname1}</span>
-							{#if result.entityname2}
-								<br />{result?.entityname2}
+				{@const pageLink = `https://iiif.ptt-archiv.ch/iiif/3/${result.file?.replaceAll(
+					'\u{0065}\u{0300}',
+					'\u{00e8}'
+				)}_${String(result.page).padStart(4, '0')}.jp2/full/max/0/default.jpg`}
+				<div class="flex min-h-24 gap-4 border-t-2 pb-4">
+					<a
+						class="flex-shrink-0"
+						href={pageLink}
+						target="_blank"
+						aria-label={m.topaz_hard_clay_mold()}
+					>
+						<img
+							src="/img/page{String(Math.round(Math.random() * (16 - 1) + 1)).padStart(2, '0')}.jpg"
+							alt={m.topaz_hard_clay_mold()}
+						/>
+					</a>
+					<div>
+						{#if result.entityname1}
+							<p>
+								<span class="font-bold">{result?.entityname1}</span>
+								{#if result.entityname2}
+									<br />{result?.entityname2}
+								{/if}
+								{#if result.street}
+									<br />{result?.street}
+								{/if}
+								{#if result.locality}
+									<br />{result?.locality}
+								{/if}
+								{#if result.phonenumber}
+									<br />Tel. {result?.phonenumber}
+								{/if}
+							</p>
+						{/if}
+						<dl class="flex flex-wrap gap-2">
+							{#if result.network}
+								<div>
+									<dt>{m.sapphire_rough_metal_bend()}</dt>
+									<dd>{result?.network}</dd>
+								</div>
 							{/if}
-							{#if result.street}
-								<br />{result?.street}
+							{#if result.file}
+								<div>
+									<dt>{m.plum_dark_night_fall()}</dt>
+									<dd>
+										<a
+											href="https://mfk.rechercheonline.ch/de/research/records/pttarchiv:{(
+												pdfIds as Record<string, number>
+											)[result.file]}"
+											target="_blank">{result.file}</a
+										>
+									</dd>
+								</div>
 							{/if}
-							{#if result.locality}
-								<br />{result?.locality}
+							{#if result.page}
+								<div>
+									<dt>{m.topaz_hard_clay_mold()}</dt>
+									<dd>
+										<a href={pageLink} target="_blank">{result?.page}</a>
+									</dd>
+								</div>
 							{/if}
-							{#if result.phonenumber}
-								<br />{result?.phonenumber}
+							{#if result.supplement}
+								<div>
+									<dt>{m.opal_rough_gravel_scatter()}</dt>
+									<dd>{result?.supplement}</dd>
+								</div>
 							{/if}
-						</p>
-					{/if}
-					<dl class="flex flex-wrap gap-2">
-						{#if result.network}
-							<div>
-								<dt>{m.sapphire_rough_metal_bend()}</dt>
-								<dd>{result?.network}</dd>
-							</div>
-						{/if}
-						{#if result.file}
-							<div>
-								<dt>{m.plum_dark_night_fall()}</dt>
-								<dd>
-									<a
-										href="https://mfk.rechercheonline.ch/de/research/records/pttarchiv:{(
-											pdfIds as Record<string, number>
-										)[result.file]}"
-										target="_blank">{result.file}</a
-									>
-								</dd>
-							</div>
-						{/if}
-						{#if result.page}
-							<div>
-								<dt>{m.topaz_hard_clay_mold()}</dt>
-								<dd>
-									<a
-										href="https://iiif.ptt-archiv.ch/iiif/3/{result.file?.replaceAll(
-											'\u{0065}\u{0300}',
-											'\u{00e8}'
-										)}_{String(result.page).padStart(4, '0')}.jp2/full/max/0/default.jpg"
-										target="_blank">{result?.page}</a
-									>
-								</dd>
-							</div>
-						{/if}
-						{#if result.supplement}
-							<div>
-								<dt>{m.opal_rough_gravel_scatter()}</dt>
-								<dd>{result?.supplement}</dd>
-							</div>
-						{/if}
-						{#if result.aggregatedcontent}
-							<div>
-								<dt>{m.periwinkle_bitter_tea_brew()}</dt>
-								<dd>{@html result?.aggregatedcontent}</dd>
-							</div>
-						{/if}
-					</dl>
+							{#if result.aggregatedcontent}
+								<div>
+									<dt>{m.periwinkle_bitter_tea_brew()}</dt>
+									<dd>{@html result?.aggregatedcontent}</dd>
+								</div>
+							{/if}
+						</dl>
+					</div>
 				</div>
 			{/each}
 		{/each}
-		<button onclick={() => (showresults += 20)}>{m.cobalt_foggy_forest_whisper()}</button>
+		<div class="flex justify-center">
+			<button
+				class="h-10 rounded-full border border-current px-4"
+				onclick={() => (showresults += 20)}>{m.cobalt_foggy_forest_whisper()}</button
+			>
+		</div>
 	</div>
 {/if}
 
@@ -351,5 +368,8 @@
 
 	a {
 		border-bottom: 1px solid #000;
+	}
+	a:has(img) {
+		border-bottom: none;
 	}
 </style>

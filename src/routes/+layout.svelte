63c0ac5b9f7e4735191dabfa323ecbd1da0bc859
flicksolutions <sebastian@flick.solutions>
2025-03-14 16:22:49 +0100

    enhance form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,13 +15,13 @@
 	</div>
 	<footer class="flex w-screen justify-around pb-3 pt-7">
 		<a href="https://www.mfk.ch/forschen/ptt-archiv" target="_blank">
-			<img class="h-[80px] w-auto" src={ptt} alt="PTT Archiv" />
+			<img class="h-[45px] w-auto md:h-[80px]" src={ptt} alt="PTT Archiv" />
 		</a>
 		<a href="https://www.dh.unibe.ch" target="_blank">
-			<img class="h-[80px] w-auto" src={unibe} alt="Universität Bern" />
+			<img class="h-[50px] w-auto md:h-[80px]" src={unibe} alt="Universität Bern" />
 		</a>
 		<a href="https://www.localsearch.ch" target="_blank">
-			<img class="h-[50px] w-auto" src={localsearch} alt="localsearch" />
+			<img class="h-[25px] w-auto md:h-[50px]" src={localsearch} alt="localsearch" />
 		</a>
 	</footer>
 </div>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as m from '$lib/paraglide/messages.js';
 	import { enhance } from '$app/forms';
+	import typewriter from '$lib/typewriter';
 	import type { PageData, ActionData } from './$types';
 	import pdfIds from './pdfIds.json';
 
@@ -61,6 +62,7 @@
 	const fromToFields = $state<string[]>([]);
 	const from = $derived(Number(fromToFields[0]));
 	const to = $derived(Number(fromToFields[1]));
+	let loading = $state(false);
 </script>
 
 <form
@@ -68,7 +70,21 @@
 	class="container mx-auto flex flex-col justify-center"
 	action="?/search"
 	data-bg-color="white"
-	use:enhance
+	use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+		// `formElement` is this `<form>` element
+		// `formData` is its `FormData` object that's about to be submitted
+		// `action` is the URL to which the form is posted
+		// calling `cancel()` will prevent the submission
+		// `submitter` is the `HTMLElement` that caused the form to be submitted
+		loading = true;
+
+		return async ({ result, update }) => {
+			// `result` is an `ActionResult` object
+			// `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
+			await update({ reset: false });
+			loading = false;
+		};
+	}}
 >
 	<div class="box mx-auto flex w-full flex-col items-center px-4 pb-10 sm:w-fit md:px-16">
 		<div class="w-[300px] md:w-[370px]">
@@ -121,138 +137,155 @@
 	</div>
 	<button
 		class="mx-auto h-10 w-fit -translate-y-5 rounded-full border border-current bg-white px-12 font-bold"
-		type="submit">{m.ochre_salty_ocean_spray()}</button
+		type="submit"
 	>
-	<!-- {#if to <= 1937 && from >= 1911} -->
-	<div class="box mx-auto flex w-full flex-col items-center px-4 pb-10 sm:w-fit md:px-16">
-		<div class="w-[300px] md:w-[370px]">
-			<h2>{m.garnet_red_gem_glow()}</h2>
-			<p>{m.moonstone_white_gem_gleam()}</p>
+		{#if loading}
+			<span in:typewriter={{ speed: 80 }}>
+				{@html m.chartreuse_cool_wind_whisper()}
+			</span>
+		{:else}
+			{m.ochre_salty_ocean_spray()}
+		{/if}
+	</button>
+	{#if to <= 1937 && from >= 1911}
+		<div class="box mx-auto flex w-full flex-col items-center px-4 pb-10 sm:w-fit md:px-16">
+			<div class="w-[300px] md:w-[370px]">
+				<h2>{m.garnet_red_gem_glow()}</h2>
+				<p>{m.moonstone_white_gem_gleam()}</p>
+			</div>
+			<input
+				class="mt-4"
+				type="text"
+				onfocus={resetFullText}
+				bind:this={textfields.entityname1}
+				maxlength="60"
+				name="entityname1"
+				placeholder={m.ruby_smooth_glass_cut()}
+			/>
+			<input
+				class="mt-4"
+				type="text"
+				onfocus={resetFullText}
+				bind:this={textfields.entityname2}
+				maxlength="60"
+				name="entityname2"
+				placeholder={m.mint_bitter_leaf_chew()}
+			/>
+			<input
+				class="mt-4"
+				type="text"
+				onfocus={resetFullText}
+				bind:this={textfields.supplement}
+				maxlength="60"
+				name="supplement"
+				placeholder={m.indigo_hot_flame_flicker()}
+			/>
+			<input
+				class="mt-4"
+				type="text"
+				onfocus={resetFullText}
+				bind:this={textfields.street}
+				maxlength="60"
+				name="street"
+				placeholder={m.agate_soft_silk_weave()}
+			/>
+			<input
+				class="mt-4"
+				type="text"
+				onfocus={resetFullText}
+				bind:this={textfields.locality}
+				maxlength="60"
+				name="locality"
+				placeholder={m.jade_dull_stone_grind()}
+			/>
+			<input
+				class="mt-4"
+				type="text"
+				onfocus={resetFullText}
+				bind:this={textfields.phonenumber}
+				maxlength="60"
+				name="phonenumber"
+				placeholder={m.lavender_dim_shadow_hide()}
+			/>
 		</div>
-		<input
-			class="mt-4"
-			type="text"
-			onfocus={resetFullText}
-			bind:this={textfields.entityname1}
-			maxlength="60"
-			name="entityname1"
-			placeholder={m.ruby_smooth_glass_cut()}
-		/>
-		<input
-			class="mt-4"
-			type="text"
-			onfocus={resetFullText}
-			bind:this={textfields.entityname2}
-			maxlength="60"
-			name="entityname2"
-			placeholder={m.mint_bitter_leaf_chew()}
-		/>
-		<input
-			class="mt-4"
-			type="text"
-			onfocus={resetFullText}
-			bind:this={textfields.supplement}
-			maxlength="60"
-			name="supplement"
-			placeholder={m.indigo_hot_flame_flicker()}
-		/>
-		<input
-			class="mt-4"
-			type="text"
-			onfocus={resetFullText}
-			bind:this={textfields.street}
-			maxlength="60"
-			name="street"
-			placeholder={m.agate_soft_silk_weave()}
-		/>
-		<input
-			class="mt-4"
-			type="text"
-			onfocus={resetFullText}
-			bind:this={textfields.locality}
-			maxlength="60"
-			name="locality"
-			placeholder={m.jade_dull_stone_grind()}
-		/>
-		<input
-			class="mt-4"
-			type="text"
-			onfocus={resetFullText}
-			bind:this={textfields.phonenumber}
-			maxlength="60"
-			name="phonenumber"
-			placeholder={m.lavender_dim_shadow_hide()}
-		/>
-	</div>
-	<button
-		class="mx-auto h-10 w-fit -translate-y-5 rounded-full border border-current bg-white px-12 font-bold"
-		type="submit">{m.ochre_salty_ocean_spray()}</button
-	>
-	<!-- {/if} -->
+		<button
+			class="mx-auto h-10 w-fit -translate-y-5 rounded-full border border-current bg-white px-12 font-bold"
+			type="submit"
+		>
+			{#if loading}
+				insert loading spinner here
+			{:else}
+				{m.ochre_salty_ocean_spray()}
+			{/if}
+		</button>
+	{/if}
 </form>
 
 {#if Object.keys(visibleResults).length > 0}
-	{#each Object.keys(visibleResults) as year}
-		<h2>{year}</h2>
-		{#each visibleResults[Number(year)] as result}
-			{#if result.entityname1}
-				<p>
-					<span class="font-bold">{result?.entityname1}</span>
-					{#if result.entityname2}
-						<br />{result?.entityname2}
+	<div class="container mx-auto">
+		{#each Object.keys(visibleResults) as year}
+			<h2 class="text-4xl">{year}</h2>
+			{#each visibleResults[Number(year)] as result}
+				<div class="border-t-2 pb-4">
+					{#if result.entityname1}
+						<p>
+							<span class="font-bold">{result?.entityname1}</span>
+							{#if result.entityname2}
+								<br />{result?.entityname2}
+							{/if}
+							{#if result.street}
+								<br />{result?.street}
+							{/if}
+							{#if result.locality}
+								<br />{result?.locality}
+							{/if}
+							{#if result.phonenumber}
+								<br />{result?.phonenumber}
+							{/if}
+						</p>
 					{/if}
-					{#if result.street}
-						<br />{result?.street}
-					{/if}
-					{#if result.locality}
-						<br />{result?.locality}
-					{/if}
-					{#if result.phonenumber}
-						<br />{result?.phonenumber}
-					{/if}
-				</p>
-			{/if}
-
-			<dl>
-				{#if result.file}
-					<dt>{m.plum_dark_night_fall()}</dt>
-					<dd>
-						<a
-							href="https://mfk.rechercheonline.ch/de/research/records/pttarchiv:{(
-								pdfIds as Record<string, number>
-							)[result.file]}"
-							target="_blank">{result.file}</a
-						>
-					</dd>
-				{/if}
-				{#if result.page}
-					<dt>{m.topaz_hard_clay_mold()}</dt>
-					<dd>
-						<a
-							href="https://iiif.ptt-archiv.ch/iiif/3/{result.file?.replaceAll(
-								'\u{0065}\u{0300}',
-								'\u{00e8}'
-							)}_{String(result.page).padStart(4, '0')}.jp2/full/max/0/default.jpg"
-							target="_blank">{result?.page}</a
-						>
-					</dd>
-				{/if}
-				{#if result.network}
-					<dt>{m.sapphire_rough_metal_bend()}</dt>
-					<dd>{result?.network}</dd>
-				{/if}
-				{#if result.supplement}
-					<dt>{m.opal_rough_gravel_scatter()}</dt>
-					<dd>{result?.supplement}</dd>
-				{/if}
-				{#if result.aggregatedcontent}
-					<dt>{m.periwinkle_bitter_tea_brew()}</dt>
-					<dd>{result?.aggregatedcontent}</dd>
-				{/if}
-			</dl>
+					<dl>
+						{#if result.file}
+							<dt>{m.plum_dark_night_fall()}</dt>
+							<dd>
+								<a
+									href="https://mfk.rechercheonline.ch/de/research/records/pttarchiv:{(
+										pdfIds as Record<string, number>
+									)[result.file]}"
+									target="_blank">{result.file}</a
+								>
+							</dd>
+						{/if}
+						{#if result.page}
+							<dt>{m.topaz_hard_clay_mold()}</dt>
+							<dd>
+								<a
+									href="https://iiif.ptt-archiv.ch/iiif/3/{result.file?.replaceAll(
+										'\u{0065}\u{0300}',
+										'\u{00e8}'
+									)}_{String(result.page).padStart(4, '0')}.jp2/full/max/0/default.jpg"
+									target="_blank">{result?.page}</a
+								>
+							</dd>
+						{/if}
+						{#if result.network}
+							<dt>{m.sapphire_rough_metal_bend()}</dt>
+							<dd>{result?.network}</dd>
+						{/if}
+						{#if result.supplement}
+							<dt>{m.opal_rough_gravel_scatter()}</dt>
+							<dd>{result?.supplement}</dd>
+						{/if}
+						{#if result.aggregatedcontent}
+							<dt>{m.periwinkle_bitter_tea_brew()}</dt>
+							<dd>{result?.aggregatedcontent}</dd>
+						{/if}
+					</dl>
+				</div>
+			{/each}
 		{/each}
-	{/each}
-	<button onclick={() => (showresults += 20)}>{m.cobalt_foggy_forest_whisper()}</button>
+		<button onclick={() => (showresults += 20)}>{m.cobalt_foggy_forest_whisper()}</button>
+	</div>
 {/if}
 
 <style lang="postcss">
@@ -261,20 +294,20 @@
 		background-color: #fdfd03;
 		border: 1px solid #848484;
 		border-radius: 1em;
+		h2 {
+			font-size: 1.5em;
+			line-height: 1.5em;
+			font-weight: 700;
+			margin: 5px 0 5px 0;
+			font-family: 'Favorit Regular', Helvetica, Arial, Geneva, Verdana, Tahoma, sans-serif;
+			@apply mt-6;
+		}
 	}
 	input[type='text'] {
 		width: 300px;
 	}
 	label span {
 		@apply pr-4;
-	}
-	h2 {
-		font-size: 1.5em;
-		line-height: 1.5em;
-		font-weight: 700;
-		margin: 5px 0 5px 0;
-		font-family: 'Favorit Regular', Helvetica, Arial, Geneva, Verdana, Tahoma, sans-serif;
-		@apply mt-6;
 	}
 	input {
 		padding: 1px 2px 1px 10px;
